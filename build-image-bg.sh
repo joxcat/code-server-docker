5977cdbd23783bade496047de6c2e0cbedd4a4d9
Johan Planchon <dev@planchon.xyz>
2022-07-05 15:08:15 +0200

    feat: updated code-server to 4.5.0 and mold to 1.3.1

--- build-image-bg.sh
+++ build-image-bg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 name=${IMAGE_NAME:-codeserver}
-nohup docker build -t "$name" dockerfile > build-image-bg.log &
+nohup docker build -t "$name" --build-arg GIT_NAME="$GIT_NAME" --build-arg GIT_EMAIL="$GIT_EMAIL" dockerfile > build-image-bg.log &
 disown
