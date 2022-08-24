353eba0c0021f53ea6454d2596a1056ebad8b569
Johan Planchon <dev@planchon.dev>
2022-08-24 13:24:24 +0200

    feat: updated dependencies and disabled docker mapping

--- build-image-bg.sh
+++ build-image-bg.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 name=${IMAGE_NAME:-codeserver}
-nohup docker build -t "$name" --build-arg GIT_NAME="$GIT_NAME" --build-arg GIT_EMAIL="$GIT_EMAIL" dockerfile > build-image-bg.log &
+with_cache=$([ -z "$NO_CACHE" ])
+# shellcheck disable=2046
+nohup docker build -t "$name" $(if ! $with_cache; then echo '--no-cache'; fi) --build-arg GIT_NAME="$GIT_NAME" --build-arg GIT_EMAIL="$GIT_EMAIL" dockerfile > build-image-bg.log &
 disown
