044eb4c9b3c5c04e4b8cffaa77a061ae8a4faafb
Johan Planchon <dev@planchon.dev>
2022-11-24 15:14:39 +0100

    fix: fixed NO_CACHE env

--- build-image-bg.sh
+++ build-image-bg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=${IMAGE_NAME:-codeserver}
-with_cache=$([ -z "$NO_CACHE" ])
+with_cache=$(test -z "$NO_CACHE" && echo "" || echo '--no-cache')
 # shellcheck disable=2046
-nohup docker build -t "$name" $(if ! $with_cache; then echo '--no-cache'; fi) --build-arg GIT_NAME="$GIT_NAME" --build-arg GIT_EMAIL="$GIT_EMAIL" dockerfile > build-image-bg.log &
+nohup docker build -t "$name" $with_cache --build-arg GIT_NAME="$GIT_NAME" --build-arg GIT_EMAIL="$GIT_EMAIL" dockerfile > build-image-bg.log &
 disown
